f6490cede42edb812c02c479192f7a0d8d49e8f8
Jack <jacktheroberts10@gmail.com>
2025-07-03 16:15:46 +0100

    Delete public directory

--- public/jsFiles/altNav.js
+++
@@ -1,6 +0,0 @@
-let navMobile = document.querySelector('.navbar-mobile');
-
-window.onload = function() {
-    navMobile.classList.add('navbar-mobile-active');
-    burger.classList.add('navbar-mobile-active');
-}
--- public/jsFiles/navbar.js
+++
@@ -1,8 +0,0 @@
-let burger = document.querySelector('.hamburger-icon');
-
-burger.addEventListener('click', function() {
-    document.querySelector('.hamburger-container').classList.toggle('active');
-    document.querySelector('.navbar-mobile').classList.toggle('navbar-active');
-    console.log('clicked!');
-})
-
